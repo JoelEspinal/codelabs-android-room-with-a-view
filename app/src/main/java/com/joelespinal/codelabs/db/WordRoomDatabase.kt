e252c0cbff86141d49bb9367047dc9580226aea5
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-01 22:00:03 -0400

    :construction: Configure RoomDatabase

--- app/src/main/java/com/joelespinal/codelabs/db/WordRoomDatabase.kt
+++ app/src/main/java/com/joelespinal/codelabs/db/WordRoomDatabase.kt
@@ -1,6 +1,13 @@
 package com.joelespinal.codelabs.db
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.joelespinal.codelabs.entities.Word
 
+@Database(
+    version = 1,
+    entities = arrayOf(Word::class),
+    exportSchema = false
+)
 abstract class WordRoomDatabase : RoomDatabase() {
 }
