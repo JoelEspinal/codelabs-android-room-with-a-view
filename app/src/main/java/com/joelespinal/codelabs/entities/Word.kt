0f88559464bd185fff4df9e7d142f579e3253eb9
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-01 21:28:10 -0400

    :building_construction: Move Word table name to constant variable

--- app/src/main/java/com/joelespinal/codelabs/entities/Word.kt
+++ app/src/main/java/com/joelespinal/codelabs/entities/Word.kt
@@ -4,7 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "word_table")
+const val TABLE_WORD = "word_table"
+
+@Entity(tableName = TABLE_WORD)
 data class Word(
     @PrimaryKey @ColumnInfo(name = "word") val word: String
 )
