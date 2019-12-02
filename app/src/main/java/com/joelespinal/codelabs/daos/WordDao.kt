5aca9e3cc9ed954584c74655743a75a36f125ac5
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-01 21:04:22 -0400

    :construction: Add insert function

--- app/src/main/java/com/joelespinal/codelabs/daos/WordDao.kt
+++ app/src/main/java/com/joelespinal/codelabs/daos/WordDao.kt
@@ -1,7 +1,18 @@
 package com.joelespinal.codelabs.daos
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.joelespinal.codelabs.entities.Word
 
 @Dao
 interface WordDao {
+
+    // TODO Getting all words ordered alphabetically
+
+    // TODO Inserting a word
+    @Insert
+    fun insert(word: Word)
+
+    //TODO Deleting all words
+
 }
