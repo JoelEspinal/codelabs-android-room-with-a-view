a8d89fcea07e484ba0ba71251951224ae9fb7acd
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-02 22:27:43 -0400

    Initialize WordRepository

--- app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
+++ app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
@@ -2,6 +2,16 @@ package com.joelespinal.codelabs.viewModels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.joelespinal.codelabs.db.WordRoomDatabase
+import com.joelespinal.codelabs.repositories.WordRepository
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+    }
+
 }
