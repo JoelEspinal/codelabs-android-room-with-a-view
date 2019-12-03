a87e9be371f8dbe760e73ceb22138cfa92b7a54b
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-02 22:17:44 -0400

    create WordViewModel class
    extends from AndroidViewModel

---
+++ app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
@@ -0,0 +1,7 @@
+package com.joelespinal.codelabs.viewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+}
