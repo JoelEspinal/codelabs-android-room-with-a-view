d4788eeb859bc0eb330bab523a8f4749b155b644
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-02 22:34:08 -0400

    Add insert function that launch repository insert coroutine

--- app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
+++ app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
@@ -3,9 +3,11 @@ package com.joelespinal.codelabs.viewModels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.joelespinal.codelabs.db.WordRoomDatabase
 import com.joelespinal.codelabs.entities.Word
 import com.joelespinal.codelabs.repositories.WordRepository
+import kotlinx.coroutines.launch
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -20,4 +22,7 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
         allWords = repository.allWords
     }
 
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
 }
