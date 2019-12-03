6734e44d92b7aa60e9cfa103ab2975df299439c2
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-02 22:29:58 -0400

    Add allWords LiveData of word list

--- app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
+++ app/src/main/java/com/joelespinal/codelabs/viewModels/WordViewModel.kt
@@ -2,16 +2,22 @@ package com.joelespinal.codelabs.viewModels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.joelespinal.codelabs.db.WordRoomDatabase
+import com.joelespinal.codelabs.entities.Word
 import com.joelespinal.codelabs.repositories.WordRepository
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository: WordRepository
 
+    val allWords: LiveData<List<Word>>
+
     init {
         val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
         repository = WordRepository(wordsDao)
+
+        allWords = repository.allWords
     }
 
 }
