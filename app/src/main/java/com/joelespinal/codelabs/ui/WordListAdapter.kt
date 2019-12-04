6d24e9aa375cedf96909ddba6dc087ff781a0c60
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-03 22:33:31 -0400

    Add LayoutInflater and require context by constructor

--- app/src/main/java/com/joelespinal/codelabs/ui/WordListAdapter.kt
+++ app/src/main/java/com/joelespinal/codelabs/ui/WordListAdapter.kt
@@ -1,5 +1,7 @@
 package com.joelespinal.codelabs.ui
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -7,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.joelespinal.codelabs.R
 import com.joelespinal.codelabs.entities.Word
 
-class WordListAdapter : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+class WordListAdapter internal constructor(context: Context) :
+    RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
 
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
     private var words = emptyList<Word>()
 
     inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
