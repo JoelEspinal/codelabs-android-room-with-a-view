2e5efb9404ddf52a6f7dce888bb502345a91b2de
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-03 22:03:58 -0400

    :building_construction: Add basic WordListAdapter structure

--- app/src/main/java/com/joelespinal/codelabs/ui/WordListAdapter.kt
+++ app/src/main/java/com/joelespinal/codelabs/ui/WordListAdapter.kt
@@ -1,11 +1,24 @@
 package com.joelespinal.codelabs.ui
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class WordListAdapter {
+class WordListAdapter : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
 
     inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
