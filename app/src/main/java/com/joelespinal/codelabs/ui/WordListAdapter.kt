0e7bca4460a96fe3ddbbe8192d5ac1643f6de590
JoelEspinal <j.espinalcarrasco@gmail.com>
2019-12-03 22:01:11 -0400

    :building_construction: Add WordLisAdapter whith it's ViewHolder

---
+++ app/src/main/java/com/joelespinal/codelabs/ui/WordListAdapter.kt
@@ -0,0 +1,11 @@
+package com.joelespinal.codelabs.ui
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class WordListAdapter {
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
